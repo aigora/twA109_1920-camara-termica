3f464d765bd8d55ede7c3ef6b6c1ae3d22972f64
Irenevm <61277793+Irenevm@users.noreply.github.com>
2020-05-02 19:50:32 +0200

    Update representar_pieza.c

--- representar_pieza.c
+++ representar_pieza.c
@@ -1,1 +1,31 @@
 // Representar pieza
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <windows.h> 
+
+// Funciones prototipo. 
+void gotoxy(int, int);
+
+// Función principal 
+int    main(void)
+{
+	 char pieza='*'; // Representación gráfica de la pieza   
+	 int f=0,c=30;  // Coordenadas iniciales de la pieza 
+	 
+	 gotoxy(c,f);  // Ubicamos el cursor    
+	 printf("%c",pieza);  // Mostramos la pieza
+	 
+	 printf ("\n");
+	 return 0;
+}
+// Funciones auxiliares visualización 
+void gotoxy (int x,int y )  
+{
+	HANDLE hcon;      
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;     
+	dwPos.X = x;
+	dwPos.Y= y;      
+	SetConsoleCursorPosition(hcon,dwPos);
+}
