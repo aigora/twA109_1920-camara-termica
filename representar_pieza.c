5d74e50f273fb20fb7effa1552909f56fea87d76
Irenevm <61277793+Irenevm@users.noreply.github.com>
2020-05-05 13:21:56 +0200

    Update representar_pieza.c

--- representar_pieza.c
+++ representar_pieza.c
@@ -3,6 +3,8 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <windows.h> 
+#include <time.h>
+#define FILAS 22
 
 // Funciones prototipo. 
 void gotoxy(int, int);
@@ -13,14 +15,37 @@ int    main(void)
 	 char pieza='*'; // Representación gráfica de la pieza   
 	 int f=0,c=30;  // Coordenadas iniciales de la pieza 
 	 
+	 clock_t t_inicio,t_actual; //Medidas del tiempo
+	 double segundos;
+	 
 	 gotoxy(c,f);  // Ubicamos el cursor    
 	 printf("%c",pieza);  // Mostramos la pieza
+	 
+	 while (f<FILAS)
+	 {
+	 	t_actual = clock();  //Tiempo inicial
+	 	
+	 	segundos= (double)(t_actual-t_inicio)/CLOCKS_PER_SEC;
+	 	
+	 	if (segundos>0.2)
+	 	{
+	 		gotoxy(c,f);
+	 		printf(" ");
+	 		
+	 		f++;
+	 	    gotoxy(c,f); //Ubicamos la pieza
+	 	    printf("%c",pieza);
+	 	    t_inicio = clock(); //Restauramos el tiempo
+	 		
+		 }	
+	 	
+	 }
 	 
 	 printf ("\n");
 	 return 0;
 }
-// Funciones auxiliares visualización 
-void gotoxy (int x,int y )  
+// Funciones 
+void gotoxy (int x,int y )  //Intercambia el orden habitual, ya que la “x” identifica la columna, mientras la coordenada “y” representa la fila
 {
 	HANDLE hcon;      
 	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
